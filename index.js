7799f683044d7913e2af671d47d6b463b82a37e2
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2024-04-24 15:08:17 +0300

    upd

--- index.js
+++ index.js
@@ -132,65 +132,15 @@
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-const http = require("http");
-const EventEmitter = require("events");
 const PORT = process.env.PORT || 5000;
+const Application = require("./framework/Application");
+const userRouter = require("./src/user-router");
+const jsonParser = require("./framework/parseJson");
 
-const emitter = new EventEmitter();
+const app = new Application();
+app.use(jsonParser);
+app.addRouter(userRouter);
 
-class Router {
-	constructor() {
-		this.endpoints = {};
-	}
-
-	request(method = "GET", path, handler) {
-		if (!this.endpoints[path]) {
-			this.endpoints[path] = {};
-		}
-
-		const endpoint = this.endpoints[path];
-
-		if (endpoint[method]) {
-			throw new Error(`[${method} по адресcу ${path} is already exist]`);
-		}
-
-		endpoint[method] = handler;
-		emitter.on(`[${path}]:[${method}]`, (req, res) => {
-			handler(req, res);
-		});
-	}
-
-	get(path, handler) {
-		this.request("GET", path, handler);
-	}
-	post(path, handler) {
-		this.request("POST", path, handler);
-	}
-	put(path, handler) {
-		this.request("PUT", path, handler);
-	}
-	delete(path, handler) {
-		this.request("DELETE", path, handler);
-	}
-}
-
-const router = new Router();
-
-router.get("/users", (req, res) => {
-	res.end(`YOU SEND REQUEST TO /USERS`);
-});
-
-router.get("/posts", (req, res) => {
-	res.end(`YOU SEND REQUEST TO /POSTS`);
-});
-
-const server = http.createServer((req, res) => {
-	const emitted = emitter.emit(`[${req.url}]:[${req.method}]`, req, res);
-	if (!emitted) {
-		res.end();
-	}
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
 	console.log(`Server started  on  PORT  ${PORT}`);
 });
---
+++ src/user-router.js
@@ -0,0 +1,21 @@
+const Router = require("../framework/Router");
+
+const router = new Router();
+
+const users = [
+	{ id: 1, name: "VT" },
+	{ id: 2, name: "OO" },
+];
+
+router.get("/users", (req, res) => {
+	res.send(users);
+});
+
+router.post("/users", (req, res) => {
+	console.log(req.body);
+    const user = req.body;
+    users.push(user);
+	res.send(users);
+});
+
+module.exports = router;
